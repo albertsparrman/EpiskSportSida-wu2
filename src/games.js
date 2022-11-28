f96b06823bd308ea2d7a86474635c76a71b2390a
albertsparrman <albert.sparrman@elev.ga.ntig.se>
2022-11-28 21:47:10 +0100

    lite off camera grinding

--- src/games.js
+++ src/games.js
@@ -1,9 +1,31 @@
 const left=document.getElementById("left");
+const right=document.getElementById("right");
+var root = document.querySelector(':root');
+let a = 0
+let b = 1
+let c = 2
 
 left.addEventListener('click', _ => {
-   document.documentElement.style.setProperty() 
+   if (a === +2) {
+
+   }
+   else {
+   root.style.setProperty('--order1', a+=1)
+   
+   root.style.setProperty('--order2', b+=1)
+
+   root.style.setProperty('--order3', c+=1)
+   }
 });
 
+
+right.addEventListener('click', _ => {   
+   root.style.setProperty('--order1', a-=1)
+
+   root.style.setProperty('--order2', b-=1)
+   
+   root.style.setProperty('--order3', c-=1)
+});
 
 
 
