0d044a99fcedf9147bb78ff2c45c4fb71edbf1fd
albertsparrman <albert.sparrman@elev.ga.ntig.se>
2022-12-20 12:01:48 +0100

    lite mer episkt

--- src/news.js
+++ src/news.js
@@ -7,10 +7,7 @@ const news4 = "Nytt avtal med Select och XXL från 2023"
 var div = document.getElementsByClassName(".news-background");
 let i = 1;
 
-
 setInterval(myFunc, 7000, "param1", "param2");
-
-setInterval(checkpoint1, 2000, "param1", "param2");
 
 function myFunc(p1, p2) {
     if (i == 0) {
@@ -19,7 +16,7 @@ function myFunc(p1, p2) {
         document.getElementById("small-news-4").style.color = "#ffffff6f"
         document.getElementById("progress1").style.backgroundColor = "white"
         document.getElementById("progress4").style.backgroundColor = "#ffffff6f"
-        document.getElementById("news").style.backgroundImage="url(../img/4UUdcc6JtM-600.jpeg)"
+        document.getElementById("news").style.backgroundImage="url(/img/4UUdcc6JtM-600.jpeg)"
         document.getElementById('bar1').style.transition="linear 7s"
         document.getElementById('bar1').style.width="100%"
         document.getElementById('bar4').style.transition="0s"
@@ -31,7 +28,7 @@ function myFunc(p1, p2) {
         document.getElementById("small-news-1").style.color = "#ffffff6f"
         document.getElementById("progress2").style.backgroundColor = "white"
         document.getElementById("progress1").style.backgroundColor = "#ffffff6f"
-        document.getElementById("news").style.backgroundImage = "url(dist/img/news2.png)"
+        document.getElementById("news").style.backgroundImage = "url(/img/news2.png)"
         document.getElementById('bar2').style.transition="linear 7s"
         document.getElementById('bar2').style.width="100%"
         document.getElementById('bar1').style.transition="0s"
@@ -43,7 +40,7 @@ function myFunc(p1, p2) {
         document.getElementById("small-news-2").style.color = "#ffffff6f"
         document.getElementById("progress3").style.backgroundColor = "white"
         document.getElementById("progress2").style.backgroundColor = "#ffffff6f"
-        document.getElementById("news").style.backgroundImage="url(../img/news3.jpg)"
+        document.getElementById("news").style.backgroundImage="url(/img/news3.jpg)"
         document.getElementById('bar3').style.transition="linear 7s"
         document.getElementById('bar3').style.width="100%"
         document.getElementById('bar2').style.transition="0s"
@@ -55,7 +52,7 @@ function myFunc(p1, p2) {
         document.getElementById("small-news-3").style.color = "#ffffff6f"
         document.getElementById("progress4").style.backgroundColor = "white"
         document.getElementById("progress3").style.backgroundColor = "#ffffff6f"
-        document.getElementById("news").style.backgroundImage="url(../img/news4.png)"
+        document.getElementById("news").style.backgroundImage="url(/img/news4.png)"
         document.getElementById('bar4').style.transition="linear 7s"
         document.getElementById('bar4').style.width="100%"
         document.getElementById('bar3').style.transition="0s"
