4e4440bf455f75ee10c27fcd0a85f3b2aca98a8b
albertsparrman <albert.sparrman@elev.ga.ntig.se>
2023-05-12 10:55:05 +0200

    Började implementera APIn

---
+++ src/_data/gamesAPI.js
@@ -0,0 +1,12 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+require('dotenv').config();
+
+module.exports = async function() {
+    let url = `https://helpful-truthful-continent.glitch.me/api/v1/workouts`;
+
+    /* This returns a promise */
+    return EleventyFetch(url, {
+        duration: "1d", // save for 1 day
+        type: "json"    // we’ll parse JSON for you
+    });
+};
