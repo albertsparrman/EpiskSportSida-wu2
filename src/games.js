0bb4a83cf7ed532d3e37225e0c0e58a66b8a2c7a
albertsparrman <albert.sparrman@elev.ga.ntig.se>
2023-05-16 15:19:35 +0200

    Saker hände

--- src/games.js
+++ src/games.js
@@ -4,18 +4,19 @@ var root = document.querySelector(':root');
 let a = 0
 let b = 1
 let c = 2
+let d = 3
+let e = 4
 
 left.addEventListener('click', _ => {
-   if (a == 0) {
-
-   }
-   else {
    root.style.setProperty('--order1', a+=1)
    
    root.style.setProperty('--order2', b+=1)
 
    root.style.setProperty('--order3', c+=1)
-   }
+   
+   root.style.setProperty('--order4', d+=1)
+   
+   root.style.setProperty('--order5', e+=1)
 });
 
 
@@ -25,6 +26,10 @@ right.addEventListener('click', _ => {
    root.style.setProperty('--order2', b-=1)
    
    root.style.setProperty('--order3', c-=1)
+
+   root.style.setProperty('--order4', d-=1)
+   
+   root.style.setProperty('--order5', e-=1)
 });
 
 
